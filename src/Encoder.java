7de028d776ab10481c0424fc976ad340bc6ab2fc
lily26bailey <lily26bailey@gmail.com>
2020-09-04 10:31:44 -0700

    changes

--- src/Encoder.java
+++ src/Encoder.java
@@ -6,21 +6,28 @@ import java.util.*;
 private HashMap<String, Integer> table;
 public class Encoder {
 
+	public Encoder()
+	{
+		
+	}
 	//fills in hashmap with initial characters and corresponding number values from ascii chart
 	public HashMap<String, Integer> fillInAsciiValues()
 	{
-		HashMap <String, Integer> map = new HashMap<String, Integer>;
+		HashMap <String, Integer> map = new HashMap<String, Integer>();
 		for (int i = 0; i < 128; i++)
 		{
 			map.put((char)i+"", i);
 		}
+		return map;
 	}
 
-	public void Encoder()
+	public void encode()
 	{
 		BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
 		PrintWriter pw = new PrintWriter ("output.txt");
+		table = fillInAsciiValues();
 		String word = "";
+		int index=128;
 		while (br.ready())
 		{
 			word+=br.read();
@@ -29,7 +36,7 @@ public class Encoder {
 				word+=br.read();
 			}
 			map.put(word, index++);
-			pw.print(map.);
+			pw.print(map.get(word.length()-2));
 		}
 	}
 
