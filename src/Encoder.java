7cb212470213c801c8384254543fde9896c362f9
lily26bailey <lily26bailey@gmail.com>
2020-09-04 10:21:20 -0700

    changes

--- src/Encoder.java
+++ src/Encoder.java
@@ -4,25 +4,30 @@ import java.io.PrintWriter;
 import java.lang.reflect.Array;
 import java.util.*;
 public class Encoder {
-	HashMap <String, Integer> map = new HashMap<String, Integer>;
-	for (int i = 0; i < 128; i++)
+	public Encoder ()
 	{
-		map.put((char)i+"", i);
 	}
-	int index = 127;
-	BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
-	PrintWriter pw = new PrintWriter ("output.txt");
-	String word = "";
-	while (br.ready())
+	public void encode()
 	{
-		word+=br.read();
-		while (map.get(word)!=null)
+		HashMap <String, Integer> map = new HashMap<String, Integer>;
+		for (int i = 0; i < 128; i++)
+		{
+			map.put((char)i+"", i);
+		}
+		int index = 127;
+		BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
+		PrintWriter pw = new PrintWriter ("output.txt");
+		String word = "";
+		while (br.ready())
 		{
 			word+=br.read();
+			while (map.get(word)!=null)
+			{
+				word+=br.read();
+			}
+			map.put(word, index++);
+			pw.print(map.get(word.substring((word.length()-2)))); 
 		}
-		map.put(word, index++);
-		pw.print(map.);
 	}
-}
 
 }
