ec3d7cb8d3ca99d34a4a3ccf140a04c1681adf5a
lily26bailey <lily26bailey@gmail.com>
2020-09-04 10:12:37 -0700

    changes

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,30 +1,28 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.*;
 public class Encoder {
-	import java.io.BufferedReader;
-	import java.io.FileReader;
-	import java.io.PrintWriter;
-	import java.lang.reflect.Array;
-	import java.util.*;
-	public class Encoder {
-		HashMap <String, Integer> map = new HashMap<String, Integer>;
-		for (int i = 0; i < 128; i++)
-		{
-			map.put((char)i+"", i);
-		}
-		int index = 127;
-		BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
-		PrintWriter pw = new PrintWriter ("output.txt");
-		String word = "";
-		while (br.ready())
+	HashMap <String, Integer> map = new HashMap<String, Integer>;
+	for (int i = 0; i < 128; i++)
+	{
+		map.put((char)i+"", i);
+	}
+	int index = 127;
+	BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
+	PrintWriter pw = new PrintWriter ("output.txt");
+	String word = "";
+	while (br.ready())
+	{
+		word+=br.read();
+		while (map.get(word)!=null)
 		{
 			word+=br.read();
-			while (map.get(word)!=null)
-			{
-				word+=br.read();
-			}
-			map.put(word, index++);
-			pw.print(map.);
 		}
+		map.put(word, index++);
+		pw.print(map.);
 	}
+}
 
 }
