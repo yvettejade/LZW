6e163eb2c6b765763e9088c80def556908bfe7cc
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-04 10:24:00 -0700

    fillInAsciiValues Method and Comment
    fills in the hashmap with ascii values 0-127 with corresponding characters

--- src/Encoder.java
+++ src/Encoder.java
@@ -3,26 +3,34 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
 import java.util.*;
+private HashMap<String, Integer> table;
 public class Encoder {
-	HashMap <String, Integer> map = new HashMap<String, Integer>;
-	for (int i = 0; i < 128; i++)
+
+	//fills in hashmap with initial characters and corresponding number values from ascii chart
+	public HashMap<String, Integer> fillInAsciiValues()
 	{
-		map.put((char)i+"", i);
+		HashMap <String, Integer> map = new HashMap<String, Integer>;
+		for (int i = 0; i < 128; i++)
+		{
+			map.put((char)i+"", i);
+		}
 	}
-	int index = 127;
-	BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
-	PrintWriter pw = new PrintWriter ("output.txt");
-	String word = "";
-	while (br.ready())
+
+	public void Encoder()
 	{
-		word+=br.read();
-		while (map.get(word)!=null)
+		BufferedReader br = new BufferedReader(new FileReader ("file.txt"));
+		PrintWriter pw = new PrintWriter ("output.txt");
+		String word = "";
+		while (br.ready())
 		{
 			word+=br.read();
+			while (map.get(word)!=null)
+			{
+				word+=br.read();
+			}
+			map.put(word, index++);
+			pw.print(map.);
 		}
-		map.put(word, index++);
-		pw.print(map.);
 	}
-}
 
 }
