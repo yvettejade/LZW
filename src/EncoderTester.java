c7192724e39242c7c34c08abb1101df197c234ba
isarodriguez9 <70167519+isarodriguez9@users.noreply.github.com>
2020-09-04 21:09:20 -0700

    making a tester and changing a variable name

--- src/EncoderTester.java
+++
@@ -1,9 +0,0 @@
-
-public class EncoderTester {
-	public static void main (String[]args)
-	{
-		Encoder test = new Encoder();
-		test.fillInAsciiValues();
-		System.out.println(test);
-	}
-}
---
+++ src/LZWEncoderTester.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class LZWEncoderTester {
+public static void main(String[]args) throws IOException
+{
+	Encoder tester = new Encoder();
+	tester.encode("Untitled 2");
+	System.out.println();
+	
+}
+}
