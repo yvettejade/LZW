f947991ce5efe592243309996c09ae1a0ef0599d
yvettejade <43455728+yvettejade@users.noreply.github.com>
2020-10-04 14:09:59 -0700

    cache update, doesn't work tho, comments coming later

--- src/riDecoder.java
+++ src/riDecoder.java
@@ -1,30 +1,74 @@
-/*
-	least recently used
-*/
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
-public class riDecoder {
+/*
+	THE BUG:
+	sometimes, once we've hit the max table size, the encoder will edit the latest time it was used, but the decoder won't
+	this probably has something to do with the order they're processed (ie place), but i just can't find it
+	it seems to happen when we have something in the table already ex: " m" and we also have " my"
+	the decoder sometimes skips the " m" and just goes straight to the " my" so the table replacements are in a different order
+	thus the decoding and encoding tables are different, changing the decoding
+*/
+	
+class Pair1
+{
+	private int times;
+	private char word;
+	Pair1 (int one, char two)
+	{
+		times = one;
+		word = two;
+	}
+	public int getTimes()
+	{
+		return times;
+	}
+	public char getWord()
+	{
+		return word;
+	}
+}
+
+public class riDecoder 
+{
+	static Comparator<Pair1> pair1Comparator = new Comparator<Pair1>() 
+	{
+        //@Override
+        public int compare(Pair1 one, Pair1 two) 
+		{
+	        return one.getTimes() - two.getTimes();
+	    }
+    };
+
 	/*
 		EDIT 1:
 		Changed to char, string so we can use utf more easily
 	*/
-	private HashMap<Character, String> codeTable;
-	public riDecoder () { 
-		 codeTable = new HashMap<Character,String>();
+	private HashMap<Integer, String> codeTable;
+
+	static final int MAX = 2147483647;
+    // priorityQueue holding the recent-ness of the char + the char
+	static PriorityQueue<Pair1> pq = new PriorityQueue<>(pair1Comparator);
+
+	static HashMap<String, Integer> latest = new HashMap<String, Integer>();
+
+	public riDecoder () 
+	{ 
+		 codeTable = new HashMap<Integer,String>();
 		 /*
 		 	EDIT 2:
 		 	In order to work with Lily and Isa's encoder, it can only init up to 128
 		 */
-		for (int i = 0; i < 128; i++) { //creates table of characters
-			codeTable.put((char)i, (char)i + "");
+		for (int i = 0; i < 128; i++) 
+		{ //creates table of characters
+			codeTable.put(i, (char)(i) + "");
+			pq.add(new Pair1(MAX, (char)(i)));
+			latest.put((char)(i)+"", MAX);
 		}
+		
 	}
-	public void decode(String inputFile, String outputFile) throws IOException{
+	public void decode(String inputFile, String outputFile) throws IOException
+	{
 		BufferedReader br = new BufferedReader(new FileReader (inputFile));
 		BufferedWriter bw = new BufferedWriter(new FileWriter (outputFile));
 		
@@ -32,8 +76,11 @@ public class riDecoder {
 			starts at 128
 		*/
 		int index=128;
+		int place = 128;
+		int counter = 0;
+
 		int currentValue = br.read();
-		String previous = codeTable.get((char)currentValue)+"";
+		String previous = codeTable.get(currentValue);
 		/*
 			EDIT 3:
 			Missing the first char of the previous block, used for the special case when LZW doesn't work + adding to the table
@@ -48,50 +95,119 @@ public class riDecoder {
 		*/
 		StringBuilder decoded = new StringBuilder("");
 		decoded.append(previous);
+		prevChar = codeTable.get(currentValue).charAt(0)+"";
 		
 		while (br.ready()) 
 		{
 			currentValue = br.read();
+
 			// if combination exists 
-			if (codeTable.containsKey((char)currentValue))
+			if (codeTable.containsKey(currentValue))
 			{ 
-				currentString = codeTable.get((char)currentValue);
-				String toAdd = previous + currentString.charAt(0);// previous combination + first letter of the one just read
+				if (index == 355)
+				{
+					//System.out.println(codeTable);
+					//System.out.println("place: "+place+" currentValue: "+currentValue+" add: "+codeTable.get(currentValue));
+				}
+				currentString = codeTable.get(currentValue);
+				String toAdd = previous+"" + currentString.charAt(0);// previous combination + first letter of the one just read
 				/*
 					EDIT 5: 
 					Should never occur, but just to be safe
 					max should be 65536 bc in utf - 8 1 char is maxxed at 65536, but bc of some weird utf rule, we're capped at 55296
 				*/
-				if(index < 55296)
-					codeTable.put((char)index, toAdd );
-				index++;
+				
 				decoded.append(currentString);
+
+				codeTable.put(place, toAdd);
+				latest.put(toAdd, counter);
+				pq.add(new Pair1(counter, (char)place));
+				counter++;
+
+				//////////////////////////////////////////////////////////////////
+				// table already contains it, so don't change that
+				// update the latest and add to the priority queue
+
+				if(currentValue >= 128)
+				{
+					//System.out.println(num+ " "+ current);
+					latest.replace(codeTable.get(currentValue), counter);
+					pq.add(new Pair1(counter, (char)currentValue));
+					counter++;
+				}
 				
 			}
-			else 
+			else //error isn't here
 			{
+				//System.out.println("ahh");
 				/*
 					need to use prevChar to constructed the missing word
 				*/
 				currentString = previous + prevChar;
 				//currentString = previous + previous.charAt(0);
 				/* 
-					Should never occur, but just to be safe
 					max should be 65536 bc in utf - 8 1 char is maxxed at 65536, but bc of some weird utf rule, we're capped at 55296
 				*/
-				if(index < 55296)
-					codeTable.put((char)index, currentString);
+				codeTable.put(place, currentString);
+				latest.put(currentString, counter);
+				pq.add(new Pair1(counter, (char)place));
+				counter++;
+
 				decoded.append(currentString);
-				index++;
 			}
+
 			/*
 				need to mark down the first char of the prev block
 			*/
 			prevChar = currentString.charAt(0)+"";
-			previous=codeTable.get((char)currentValue);
+			previous=codeTable.get(currentValue);
+
+
+			//if(codeTable.size()==254)
+			//System.out.println(latest);
+
+			if(codeTable.size()>=55296) //index >= 256-1 //55296
+			{
+				Pair1 x = pq.poll();
+				//System.out.println((int)x.getWord()+ " "+x.getTimes()+ " "+codeTable.get((int)x.getWord()));
+
+				//!codeTable.containsKey((int)x.getWord()) || !latest.containsKey(codeTable.get((int)x.getWord())) ||
+				while(!codeTable.containsKey((int)x.getWord()) || !latest.containsKey(codeTable.get((int)x.getWord())) || latest.get(codeTable.get((int)x.getWord())) != x.getTimes() || (int)x.getWord() < 128)
+				{
+					x = pq.poll();
+					//System.out.println((int)x.getWord()+ " "+x.getTimes()+ " "+codeTable.get((int)x.getWord()));
+				}
+
+				//System.out.println((int)x.getWord());
+				if((int)x.getWord()==227) //133
+				{
+					//System.out.println(latest);
+					//System.out.println("ahh: "+codeTable.get((int)x.getWord()));
+					//System.out.println(codeTable);
+				}
+				//System.out.println(pq.size());
+
+				latest.remove(codeTable.get((int)x.getWord()));
+				//System.out.println(latest.size());
+
+				//System.out.println("to remove: "+table.get((int)prev)+" "+prevChar);
+
+
+				codeTable.remove((int)x.getWord());
+				place = (int)x.getWord()-1;
+			}
+			index++;
+			place++;
+			
+			//if(index == 355)
+				//{}
 			
 		}
-		
+		//System.out.println(codeTable.size());
+		//System.out.println(codeTable);
+
+		//System.out.println(index);
+
 		bw.write(decoded.toString());
 		br.close();
 		bw.close();
--- src/riEncoder.java
+++ src/riEncoder.java
@@ -1,30 +1,76 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 
-public class riEncoder {
+/*
+	THE BUG:
+	sometimes, once we've hit the max table size, the encoder will edit the latest time it was used, but the decoder won't
+	this probably has something to do with the order they're processed (ie place), but i just can't find it
+	it seems to happen when we have something in the table already ex: " m" and we also have " my"
+	the decoder sometimes skips the " m" and just goes straight to the " my" so the table replacements are in a different order
+	thus the decoding and encoding tables are different, changing the decoding
+*/
+class Pair
+{
+	private int times;
+	private String word;
+	Pair (int one, String two)
+	{
+		times = one;
+		word = two;
+	}
+	public int getTimes()
+	{
+		return times;
+	}
+	public String getWord()
+	{
+		return word;
+	}
+}
+
+public class riEncoder 
+{
+
+	static Comparator<Pair> pairComparator = new Comparator<Pair>() 
+	{
+        //@Override
+        public int compare(Pair one, Pair two) 
+		{
+	        return one.getTimes() - two.getTimes();
+	    }
+    };
+
+    static final int MAX = 2147483647;
+	// priorityQueue holding the recent-ness of the char + the char
+	static PriorityQueue<Pair> pq = new PriorityQueue<>(pairComparator);
+
+	static HashMap<String, Integer> latest = new HashMap<String, Integer>();
+
 	/*
 		EDIT 1:
 		changed HashMap<String, Integer> to HashMap<String, Character> so we can use ascii/unicode for max compression
 	*/
-	private HashMap<String, Character> tableOfCodes;
+	private HashMap<String, Integer> tableOfCodes;
 	//constructs Encoder
 	public riEncoder()
 	{
 
 	}
 	//fills in hashmap with initial characters and corresponding number values from ascii chart(0-127)
-	public HashMap<String, Character> fillInAsciiValues()
+	public HashMap<String, Integer> fillInAsciiValues()
 	{
-		tableOfCodes = new HashMap<String, Character>();
+		tableOfCodes = new HashMap<String, Integer>();
 		for (int i = 0; i < 128; i++)
 		{
 			/*
 				hashmap update
 			*/
-			tableOfCodes.put(((char)i)+"", (char)i);
+			tableOfCodes.put(((char)(i))+"", i);
+			// add inits to the priorityQueue
+			// we REALLY don't want to get rid of the inits bc it's inconvenient, so set them to the max int, so they never get hit
+			pq.add(new Pair(MAX, (char)(i)+""));
+			latest.put((char)(i)+"", MAX);
+			
 		}
 		return tableOfCodes;
 	}
@@ -36,47 +82,79 @@ public class riEncoder {
 		//fills in hashmap with initial characters from method above
 		this.tableOfCodes = fillInAsciiValues(); 
 		String currentChars = ""; 
-		int index=127;
+		int index = 128;
+		int place = 128;
+		int counter = 0;
 		while (br.ready())
 		{
+			//System.out.println(place);
 			currentChars+=(char)br.read();
 			//generates a string that isn't a key in tableOfCodes yet
-			while (tableOfCodes.get(currentChars)!=null)
+			if(tableOfCodes.containsKey(currentChars))
 			{
-				currentChars+=(char)br.read();
+				if(currentChars.length()>1)
+				{
+					latest.replace(currentChars, counter);
+					pq.add(new Pair(counter, currentChars));
+					counter++;
+				}
 			}
 			//if the size of the hashmap exceeds 2000, values will no longer be added to the table 
 			/*
 				EDIT 2:
 				max should be 65536 bc in utf - 8 1 char is maxxed at 65536, but bc of some weird utf rule, we're capped at 55296 (55295 bc > obvs)
 			*/
-			if (tableOfCodes.size()>55295) 
-			{
-				String key = currentChars.substring(0, currentChars.length()-1);
-				pw.print(tableOfCodes.get(key));
-				currentChars=currentChars.substring(currentChars.length()-1);
-			}
-			//adds new key and value to tableOfCodes
-			//prints the Integer value that corresponds to the latest recognizable key
-			//resets currentChars to start with its last letter
 			else
 			{
-				String key = currentChars.substring(0,currentChars.length()-1); 
-				index++;
-				/*
-					hashmap update
-				*/
-				tableOfCodes.put(currentChars, (char)index); 
-				pw.print(tableOfCodes.get(key));
-				currentChars=currentChars.substring(currentChars.length()-1); 
+				//adds new key and value to tableOfCodes
+				//prints the Integer value that corresponds to the latest recognizable key
+				//resets currentChars to start with its last letter
+
+					String key = currentChars.substring(0,currentChars.length()-1); 
+					
+					/*
+						hashmap update
+					*/
+					tableOfCodes.put(currentChars, place); 
+					int x = tableOfCodes.get(key);
+					pw.print((char)x);
+
+					latest.put(currentChars, counter);
+					pq.add(new Pair(counter, currentChars));
+					counter++;
+
+					currentChars=currentChars.substring(currentChars.length()-1); 
+					place++;
+			}
+
+			if (tableOfCodes.size()>=55296)//256
+			{
+				Pair x = pq.poll();
+				//System.out.println(tableOfCodes.get(x.getWord())+ " "+x.getTimes()+ " "+ x.getWord());
+
+				//System.out.println(x.getWord());
+				//System.out.println(tableOfCodes);
+				while(latest.get(x.getWord()) != x.getTimes() || tableOfCodes.get(x.getWord()) < 128)
+				{
+					x = pq.poll();
+					//System.out.println(tableOfCodes.get(x.getWord())+ " "+x.getTimes()+ " "+ x.getWord());
+				}
+
+				//tableOfCodes.put(currentChars, (int)tableOfCodes.get(x.getWord()));
+				place = tableOfCodes.get(x.getWord());
+				tableOfCodes.remove(x.getWord());
+				latest.remove(x.getWord());
 			}
 		}
+		
 		/*
 			EDIT 3: 
 			The encoder kept dropping the last character bc the while loop runs 1 time too few
 			It needs to print whatever it's holding in currentChars (the last char of the file)
 		*/
-		pw.print(tableOfCodes.get(currentChars));
+		int x = tableOfCodes.get(currentChars);
+		pw.print((char)x);
+		//System.out.println(tableOfCodes);
 		br.close();
 		pw.close();
 
